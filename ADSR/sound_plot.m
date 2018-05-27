3fd26657a6b3e6ea36f85f05cb72b54b986fe5cf
Daniel McGrath <demcgrath2@gmail.com>
2018-05-27 15:56:44 -0700

    Update sound_plot.m

--- ADSR/sound_plot.m
+++ ADSR/sound_plot.m
@@ -1,12 +1,15 @@
-%plotting
+% for plotting tones before and after ADSR modulation
 
 figure();
+
 x = note(44,1);
 t = 0 : 1/8192 : 1;
+
 subplot(2,1,1)
 plot(t,x);
 title('ADSR Profile E(t)');
 xlim([0 1]);
+
 subplot(2,1,2)
 plot(t,Envelope(Envelope(x)));
 title('ADSR Profile E(t)^{2}');
