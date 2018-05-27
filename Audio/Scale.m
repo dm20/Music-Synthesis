e80f93933fa75f5595b315cad28b447d7191be13
Daniel McGrath <demcgrath2@gmail.com>
2018-05-27 16:02:06 -0700

    Update Scale.m

--- Audio/Scale.m
+++ Audio/Scale.m
@@ -5,10 +5,10 @@
 % This function plays a scale by creating a vector of notes and then playing the vector.
 
 function scale()
-      scale = [40 42 44 45 47 49 51 52];  % the scale keynums
-      notes = [];          % empty notes vector to be passed to soundsc()
+      scale = [40 42 44 45 47 49 51 52];  % the scale's keynums
+      notes = [];          
       for keynum = scale   % iterate through each element in the scale 
           notes = [notes note(keynum,1)]; % populate the notes vector
       end
-      soundsc(notes)       % play the scale at the sampling rate
+      soundsc(notes)       % play the scale
 end
